0e179a941cdba0cba0d1622052da0320cd52b46d
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-16 19:44:18 -0300

    update main

--- main.py
+++ main.py
@@ -6,8 +6,21 @@ def sheet_teste_integracao():
     set_title(*titles)
 
 
+def sheet_nu_ago():
+    from sheet_nu_ago.spreads import (
+        convert_values_sheet,
+        calculate_expense,
+        calculate_revenue,
+    )
+
+    # convert_values_sheet()
+    calculate_expense()
+    calculate_revenue()
+
+
 def main():
-    sheet_teste_integracao()
+    # sheet_teste_integracao()
+    sheet_nu_ago()
 
 
 if __name__ == "__main__":
