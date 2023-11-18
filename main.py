7fe9c4aa9dfc8a27e00e9f9a41d86c90d4999dd8
Desktop Alexandre <desktopalexandre@gmail.com>
2023-11-17 21:05:30 -0300

    done

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ def sheet_teste_integracao():
 
 
 def sheet_nu_ago():
-    from sheet_nu_ago.spreads import (
-        convert_values_sheet,
-        calculate_expense,
-        calculate_revenue,
-    )
+    from sheet_nu_ago import key, spreads, services
+    from utils import worksheet
 
-    # convert_values_sheet()
-    calculate_revenue()
-    calculate_expense()
+    # ws = worksheet(key, 8)
+
+    services.update_all_pages()
+
+    # spreads.calculate_revenue()
+    # spreads.calculate_expense()
 
 
 def main():
