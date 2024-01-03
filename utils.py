c97dbaf509fcdee0eabc95b45cc5b96aad614ff1
alexandre <alexandrealvescs@gmail.com>
2024-01-03 20:51:05 -0300

    feat: new function create template creating spreadsheet title automatically

--- utils.py
+++ utils.py
@@ -24,3 +24,36 @@ def worksheet(key, page=0):
     worksheet = wks.get_worksheet(page)
 
     return worksheet
+
+
+def create_template(key: str):
+    scope = ["https://spreadsheets.google.com/feeds"]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        "credentials.json", scope
+    )
+
+    gc = gspread.authorize(credentials)
+
+    wks = gc.open_by_key(key)
+
+    month_list = [
+        "JAN",
+        "FEV",
+        "MAR",
+        "ABR",
+        "MAI",
+        "JUN",
+        "JUL",
+        "AGO",
+        "SET",
+        "OUT",
+        "NOV",
+        "DEZ",
+    ]
+
+    for month in month_list:
+        wks.add_worksheet(f"{month}_EXTRATO", 1000, 26)
+
+    for month in month_list:
+        wks.add_worksheet(f"{month}_CRED", 1000, 26)
