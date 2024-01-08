88f3f09aef702584132f682e5c3ceac21bebeaac
alexandre <alexandrealvescs@gmail.com>
2024-01-08 16:44:43 -0300

    refactor: clean code and update on get credentials

--- utils.py
+++ utils.py
@@ -1,26 +1,16 @@
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import google.auth
 
 
 def worksheet(key, page=0):
-    # Escopo utilizado
     scope = ["https://spreadsheets.google.com/feeds"]
 
-    # Dados de autenticação
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials.json", scope
-    )
+    credentials, _ = google.auth.default(scope)
 
-    # Se autentica
     gc = gspread.authorize(credentials)
 
-    # Abre a planilha
     wks = gc.open_by_key(key)
 
-    # Para selecionar a planilha pelo o nome use o código abaixo
-    # wks = gc.open('Teste Python')
-
-    # Seleciona a primeira página da planilha
     worksheet = wks.get_worksheet(page)
 
     return worksheet
@@ -29,9 +19,7 @@ def worksheet(key, page=0):
 def create_template(key: str):
     scope = ["https://spreadsheets.google.com/feeds"]
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials.json", scope
-    )
+    credentials, _ = google.auth.default(scope)
 
     gc = gspread.authorize(credentials)
 
