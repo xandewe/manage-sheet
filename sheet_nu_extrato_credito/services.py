23de3ea8a765d7656896330a4cdcd2c4e63b52cc
alexandre <alexandrealvescs@gmail.com>
2024-01-09 19:13:45 -0300

    feat: new funcionality created populate_database_with_credit

--- sheet_nu_extrato_credito/services.py
+++ sheet_nu_extrato_credito/services.py
@@ -1,6 +1,12 @@
 from gspread import WorksheetNotFound
 from . import spreads
 from time import sleep
+import csv
+import os
+from decimal import Decimal
+import requests
+from . import url
+
 
 def update_all_pages():
     month = 1
@@ -18,3 +24,47 @@ def update_all_pages():
 
         except WorksheetNotFound as _:
             print(f"WorkSheet do mês {index} não encontrada!")
+
+
+def populate_database_with_credit(file_csv: str):
+    file_csv = file_csv.split(".")[0]
+    file_path = os.path.join("./package_csv", f"{file_csv}.csv")
+
+    counter = 0
+    counter_payment = 0
+
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+
+            for line in reader:
+                if reader.line_num > 1:
+                    if line[1] != "payment":
+                        fields = ["description", "name", "value"]
+                        date_purchase = line.pop(0)
+
+                        data = dict(zip(fields, line))
+                        data["value"] = Decimal(data["value"])
+                        data.update(
+                            {"description": f'{date_purchase} {data["description"]}'}
+                        )
+                        data.update({"type": "Credit Card"})
+                        data.update({"status": "Done"})
+                        _, year, month = file_csv.split("-")
+                        data.update({"created_at": f"{year}-{month}-01"})
+
+                        response = requests.post(url, data, verify=False)
+
+                        counter += 1
+
+                    else:
+                        counter_payment += 1
+
+                    print(f"Processando ...")
+
+            print(
+                f"\n{counter+counter_payment} dados processados para o DB com sucesso\n{counter_payment} dados de pagamento\n{counter} dados de saída"
+            )
+
+    else:
+        print(f"Arquivo não encontrado no diretório | {file_path}")
