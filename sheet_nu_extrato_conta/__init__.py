30183f02702ab2dc1981e7a78392cd0bf4c5a67e
alexandre <alexandrealvescs@gmail.com>
2024-02-06 18:35:52 -0300

    feat: get SAVE_CSV_PATH environment variable

--- sheet_nu_extrato_conta/__init__.py
+++ sheet_nu_extrato_conta/__init__.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 key = os.getenv("SHEET_NU_EXTRATO")
 url = os.getenv("URL")
+csv_path = os.getenv("SAVE_CSV_PATH")
 
 CASA = ["COMPANHIA PAULISTA DE FORCA E LUZ", "CLARO", ]
 TRANSPORTE = ["UBER"]
