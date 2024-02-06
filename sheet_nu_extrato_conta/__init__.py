69003921003d17fbec868d378006cb3c84c0054a
alexandre <alexandrealvescs@gmail.com>
2024-02-06 15:02:37 -0300

    feat: constant variable  added for fill automatic tags and sub tags

--- sheet_nu_extrato_conta/__init__.py
+++ sheet_nu_extrato_conta/__init__.py
@@ -5,3 +5,7 @@ load_dotenv()
 
 key = os.getenv("SHEET_NU_EXTRATO")
 url = os.getenv("URL")
+
+CASA = ["COMPANHIA PAULISTA DE FORCA E LUZ", "CLARO", ]
+TRANSPORTE = ["UBER"]
+ALIMENTACAO = ["TAUSTE", "SWIFT", "KAWAKAMI", "IFOOD"]
