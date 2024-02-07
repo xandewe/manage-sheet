fd24451c143f79b2a78497fae9380c9e67adaf49
alexandre <alexandrealvescs@gmail.com>
2024-02-07 16:32:30 -0300

    feat: moving standard_month variable and standard_year variable for init file

--- sheet_nu_extrato_conta/__init__.py
+++ sheet_nu_extrato_conta/__init__.py
@@ -1,11 +1,14 @@
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv()
 
 key = os.getenv("SHEET_NU_EXTRATO")
 url = os.getenv("URL")
 csv_path = os.getenv("SAVE_CSV_PATH")
+standard_month = str(datetime.now().month)
+standard_year = str(datetime.now().year)
 
 CASA = ["COMPANHIA PAULISTA DE FORCA E LUZ", "CLARO", ]
 TRANSPORTE = ["UBER"]
