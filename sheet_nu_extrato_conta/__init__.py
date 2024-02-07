d3eb2ba81463e3d0cf02cf03ea1319805f1d92a5
alexandre <alexandrealvescs@gmail.com>
2024-02-07 11:27:31 -0300

    feat: added constant variables

--- sheet_nu_extrato_conta/__init__.py
+++ sheet_nu_extrato_conta/__init__.py
@@ -10,3 +10,20 @@ csv_path = os.getenv("SAVE_CSV_PATH")
 CASA = ["COMPANHIA PAULISTA DE FORCA E LUZ", "CLARO", ]
 TRANSPORTE = ["UBER"]
 ALIMENTACAO = ["TAUSTE", "SWIFT", "KAWAKAMI", "IFOOD"]
+
+MONTH_LIST = [
+        "JAN",
+        "FEV",
+        "MAR",
+        "ABR",
+        "MAI",
+        "JUN",
+        "JUL",
+        "AGO",
+        "SET",
+        "OUT",
+        "NOV",
+        "DEZ",
+    ]
+
+PACKAGE_PATH = "./package_csv"
