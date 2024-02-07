e01286da8e7a866fdcdb951d8a25541480e2f75e
alexandre <alexandrealvescs@gmail.com>
2024-02-07 15:01:10 -0300

    feat: verify_sheet function created

--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 import gspread
 import google.auth
 from gspread.exceptions import SpreadsheetNotFound
-
-# from sheet_nu_extrato_conta import key
 from exceptions import WorksheetException
 from sheet_nu_extrato_conta import MONTH_LIST
 
@@ -35,3 +33,23 @@ def create_template(key: str):
 
     for month in MONTH_LIST:
         wks.add_worksheet(f"{month}_CRED", 1000, 26)
+
+
+def verify_sheet(page: int, key: str):
+    """
+    return: is sliced ​​to remove the header and return only the data
+    """
+    try:
+        ws = worksheet(key, page)
+
+    except SpreadsheetNotFound as _:
+        raise WorksheetException(
+            f"\nSheet não encontrado! Verifique se a key está correta <{key}>\n"
+        )
+
+    values_list = ws.get_values()
+
+    if not values_list:
+        raise WorksheetException(f"Dados não encontrados!")
+
+    return values_list[1:]
