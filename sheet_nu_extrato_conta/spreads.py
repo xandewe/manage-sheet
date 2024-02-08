781d2f5e9f1e90c1e5718cd059c29be2e316a243
alexandre <alexandrealvescs@gmail.com>
2024-02-08 11:09:21 -0300

    refactor: cash_inflows_and_outflows_analysis function created

--- sheet_nu_extrato_conta/spreads.py
+++ sheet_nu_extrato_conta/spreads.py
@@ -28,9 +28,13 @@ def write_sheet_headers(ws: Worksheet):
     ws.update_cell(1, 12, "Investido")
 
 
-def calculate_expense(ws: Worksheet, values_list: list):
-    fmt = cell_format(backgroundColor=color(206 / 255, 76 / 255, 61 / 255))
+def cash_inflows_and_outflows_analysis(ws: Worksheet, values_list: list):
+    fmt_cash_outflow = cell_format(backgroundColor=color(206 / 255, 76 / 255, 61 / 255))
+    fmt_cash_inflow = cell_format(backgroundColor=color(78 / 255, 127 / 255, 25 / 255))
 
+    income = 0
+    return_money = 0
+    rescue = 0
     expense = 0
     invoice_card = 0
     invested = 0
@@ -40,57 +44,33 @@ def calculate_expense(ws: Worksheet, values_list: list):
         date, value, id, description, *_ = item
         convert_number = Decimal(value)
 
-        if "aplicação rdb" in description.lower():
+        if "aplicação rdb" in description.lower() and convert_number < 0:
             invested += convert_number
-            format_cell_range(ws, f"A{line}:D{line}", fmt)
+            format_cell_range(ws, f"A{line}:D{line}", fmt_cash_outflow)
 
-        elif "pagamento de fatura" in description.lower():
+        elif "pagamento de fatura" in description.lower() and convert_number < 0:
             invoice_card += convert_number
-            format_cell_range(ws, f"A{line}:D{line}", fmt)
+            format_cell_range(ws, f"A{line}:D{line}", fmt_cash_outflow)
 
         elif convert_number < 0:
             expense += convert_number
-            format_cell_range(ws, f"A{line}:D{line}", fmt)
-
-        line += 1
-
-    with localcontext() as ctx:
-        ctx.prec = 10
-        expense_convert = float(expense)
-        invoice_card_convert = float(invoice_card)
-        invested_convert = float(invested)
-
-    ws.update_cell(2, 10, expense_convert)
-    ws.update_cell(2, 11, invoice_card_convert)
-    ws.update_cell(2, 12, invested_convert)
-
-
-def calculate_income(ws: Worksheet, values_list: list):
-    fmt = cell_format(backgroundColor=color(78 / 255, 127 / 255, 25 / 255))
-
-    income = 0
-    return_money = 0
-    rescue = 0
-    line = 2
-
-    for item in values_list:
-        date, value, id, description, *_ = item
-        convert_number = Decimal(value)
+            format_cell_range(ws, f"A{line}:D{line}", fmt_cash_outflow)
 
         if (
             "estorno" in description.lower()
             or "reembolso recebido" in description.lower()
+            and convert_number > 0
         ):
             return_money += convert_number
-            format_cell_range(ws, f"A{line}:D{line}", fmt)
+            format_cell_range(ws, f"A{line}:D{line}", fmt_cash_inflow)
 
-        elif "resgate" in description.lower():
+        elif "resgate" in description.lower() and convert_number > 0:
             rescue += convert_number
-            format_cell_range(ws, f"A{line}:D{line}", fmt)
+            format_cell_range(ws, f"A{line}:D{line}", fmt_cash_inflow)
 
         elif convert_number > 0:
             income += convert_number
-            format_cell_range(ws, f"A{line}:D{line}", fmt)
+            format_cell_range(ws, f"A{line}:D{line}", fmt_cash_inflow)
 
         line += 1
 
@@ -99,7 +79,22 @@ def calculate_income(ws: Worksheet, values_list: list):
         income_convert = float(income)
         return_money_convert = float(return_money)
         rescue_convert = float(rescue)
+        expense_convert = float(expense)
+        invoice_card_convert = float(invoice_card)
+        invested_convert = float(invested)
 
     ws.update_cell(2, 7, income_convert)
     ws.update_cell(2, 8, return_money_convert)
     ws.update_cell(2, 9, rescue_convert)
+    ws.update_cell(2, 10, expense_convert)
+    ws.update_cell(2, 11, invoice_card_convert)
+    ws.update_cell(2, 12, invested_convert)
+
+    return (
+        income_convert,
+        return_money_convert,
+        rescue_convert,
+        expense_convert,
+        invoice_card_convert,
+        invested_convert,
+    )
