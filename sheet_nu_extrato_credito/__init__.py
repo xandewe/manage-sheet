9a2fbceb2e0e7d5185152171a9f7ef1452bb3506
alexandre <alexandrealvescs@gmail.com>
2024-02-12 11:39:13 -0300

    feat: constant variable SHEET_COLUMNS_NAME created

--- sheet_nu_extrato_credito/__init__.py
+++ sheet_nu_extrato_credito/__init__.py
@@ -5,3 +5,12 @@ load_dotenv()
 
 key = os.getenv("SHEET_NU_EXTRATO")
 url = os.getenv("URL")
+
+SHEET_COLUMNS_NAME = (
+    "Sub Tag",
+    "Tag",
+    "Total gasto",
+    "Pagamento",
+    "Desconto Ant.",
+)
+
